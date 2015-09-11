9073d5d53124457f3afc7c249f93d1a784f61e50
Lisa Westlund <lw222gu@student.lnu.se>
2015-09-11 16:36:08 +0200

    Started working on showing servertime

--- Login_1DV608-master/view/DateTimeView.php
+++ Login_1DV608-master/view/DateTimeView.php
@@ -5,8 +5,8 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
-
+        date_default_timezone_set('Europe/Stockholm');
+        $timeString = date("l jS \of F Y") . ", the time is " . date("H:i:s.");
 		return '<p>' . $timeString . '</p>';
 	}
 }
